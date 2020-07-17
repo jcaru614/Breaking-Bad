ea0e3d5a1fdf9b880906353ddf00809ac5cdad05
Joseph Caruana <josephcaruana@Josephs-MacBook-Pro.local>
2020-07-17 12:28:33 -0700

    commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, Image, Button, ScrollView, TouchableHighlight, Modal, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getBreakingBad();
+  }, [])
+
+  const getBreakingBad = () => {
+    axios.get("https://www.breakingbadapi.com/api/characters")
+      .then(res => setCharacters(res.data))
+      .catch(err => console.log(err))
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <Text>I am the one who knocks!</Text>
+        <ScrollView>
+        { characters ?
+          characters.map ((char, index) => {
+            return (
+              <View>
+              <Text key={char.char_id}>{char.name}</Text>
+              </View>
+              
+            )
+          }) : null
+        }
+        </ScrollView>
+        
+      </SafeAreaView>
+
     </View>
   );
 }
@@ -14,8 +43,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: 'gray',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+
   },
 });
